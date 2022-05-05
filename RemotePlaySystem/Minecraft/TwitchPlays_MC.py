46786c0735779fbe4054a5d15333f187cfa21cdb
Olivia <wild_hosikuzu@icloud.com>
2022-05-05 16:18:51 +0900

    新旧

--- RemotePlaySystem/Minecraft/TwitchPlays_MC.py
+++ RemotePlaySystem/Minecraft/TwitchPlays_MC.py
@@ -1,3 +1,4 @@
+import win32gui
 import socket
 import threading
 import time
@@ -6,6 +7,15 @@ import mouse
 import keyboard
 import datetime
 import csv
+
+hwnd = win32gui.FindWindow(None, 'Minecraft* 1.15.2') #ここ！！変更の必要あり！！
+win32gui.SetForegroundWindow(hwnd)
+x,y,height,width = win32gui.GetWindowRect(hwnd)
+#772 368 1650 887
+print('------------------')
+print(x,y,width,height) #ここも間違ってそう
+print('------------------')
+
 #from ahk import AHK
 #ahk = AHK(executable_path='C:\\Program Files\\AutoHotkey\\AutoHotkey.exe')
 
@@ -13,16 +23,16 @@ SERVER = "irc.twitch.tv" #固定
 PORT = 6667 #固定
 
 #Your OAUTH Code Here https://twitchapps.com/tmi/
-PASS = "" #自分のTwitchアカから取れるので、変える(変更DONE)
-
+PASS = "oauth:hm336j4zk99b9121e1nutixh1oghx1" #自分のTwitchアカから取れるので、変える(変更DONE)
+#こちらに変更かもoauth:qo79khjfjstpusa8mar51aqdgikgjw
 #What you'd like to name your bot
 BOT = "ICE_MCI_testBot"
 
 #The channel you want to monitor
-CHANNEL = "" #自分のチャンネルの名前
+CHANNEL = "it0sat0" #自分のチャンネルの名前
 
 #Your account
-OWNER = "" #自分のチャンネルの名前
+OWNER = "it0sat0" #自分のチャンネルの名前
 
 message = ""
 user = ""
@@ -37,7 +47,7 @@ irc.send((	"PASS " + PASS + "\n" +
 TimeStamp = []
 command = []
 
-playername = ""  #ちゃんと変更すること
+playername = "it0sat0sub"  #ちゃんと変更すること
 
 def gamecontrol():  #あまり気にしなくていい
 	global message
@@ -45,7 +55,7 @@ def gamecontrol():  #あまり気にしなくていい
 		time.sleep(0.001)
 		#mousemoving.offset_move(mouse_offset_x, mouse_offset_y)
 		if playername == user.lower():
-			if "st" == message.lower(): #Minecraft用のコマンドたち
+			if "st" == message.lower(): #旧システムのコマンド
 				mouse.move(0, 0, absolute=False, duration=0.1)
 				keypresser.key_up('w')
 				keypresser.key_up('a')
@@ -53,10 +63,10 @@ def gamecontrol():  #あまり気にしなくていい
 				keypresser.key_up('d')
 				message = ""
 			elif "vw" == message.lower():
-				mouse.move(0, -2, absolute=False, duration=0.5)
+				mouse.move(0, -5, absolute=False, duration=0.5)
 				message = ""
 			elif "vs" == message.lower():
-				mouse.move(0, 2, absolute=False, duration=0.5)
+				mouse.move(0, 5, absolute=False, duration=0.5)
 				message = ""
 			elif "va" == message.lower():
 				mouse.move(-5, 0, absolute=False, duration=0.5)
@@ -65,45 +75,33 @@ def gamecontrol():  #あまり気にしなくていい
 				mouse.move(5, 0, absolute=False, duration=0.5)
 				message = ""
 			elif "mw" == message.lower():
-				mouse.move(0, -30, absolute=False, duration=0.5)
-				message = ""
-			elif "ms" == message.lower():
-				mouse.move(0, 30, absolute=False, duration=0.5)
+				mouse.move(0, -15, absolute=False, duration=0.5)
 				message = ""
 			elif "ma" == message.lower():
-				mouse.move(-30, 0, absolute=False, duration=0.5)
+				mouse.move(-15, 0, absolute=False, duration=0.5)
+				message = ""
+			elif "ms" == message.lower():
+				mouse.move(0, 15, absolute=False, duration=0.5)
 				message = ""
 			elif "md" == message.lower():
-				mouse.move(30, 0, absolute=False, duration=0.5)
+				mouse.move(15, 0, absolute=False, duration=0.5)
 				message = ""
 			elif "w" == message.lower():
-				keypresser.key_down('w')
-				message = ""
-			elif "a" == message.lower():
-				keypresser.key_down('a')
-				message = ""
-			elif "s" == message.lower():
-				keypresser.key_down('s')
-				message = ""
-			elif "d" == message.lower():
-				keypresser.key_down('d')
-				message = ""
-			elif "ow" == message.lower():
 				keypresser.key_down('w')
 				time.sleep(1)
 				keypresser.key_up('w')
 				message = ""
-			elif "oa" == message.lower():
+			elif "a" == message.lower():
 				keypresser.key_down('a')
 				time.sleep(1)
 				keypresser.key_up('a')
 				message = ""
-			elif "os" == message.lower():
+			elif "s" == message.lower():
 				keypresser.key_down('s')
 				time.sleep(1)
 				keypresser.key_up('s')
 				message = ""
-			elif "od" == message.lower():
+			elif "d" == message.lower():
 				keypresser.key_down('d')
 				time.sleep(1)
 				keypresser.key_up('d')
@@ -112,13 +110,97 @@ def gamecontrol():  #あまり気にしなくていい
 				mouse.click()
 				message = ""
 			elif "op" == message.lower():
+				time.sleep(0.2)
 				mouse.right_click()
 				message = ""
-			elif "cl" == message.lower():
+			elif "ow" == message.lower():
+				keypresser.key_down('w')
+				time.sleep(0.5)
+				keypresser.key_up('w')
+				message = ""
+			elif "oa" == message.lower():
+				keypresser.key_down('a')
+				time.sleep(0.5)
+				keypresser.key_up('a')
+				message = ""
+			elif "os" == message.lower():
+				keypresser.key_down('s')
+				time.sleep(0.5)
+				keypresser.key_up('s')
+				message = ""
+			elif "od" == message.lower():
+				keypresser.key_down('d')
+				time.sleep(0.5)
+				keypresser.key_up('d')
+				message = ""
+			elif "cl" == message.lower(): #旧、新どちらにも使用する
 				keypresser.key_down('e')
 				keypresser.key_up('e')
 				message = ""
-
+			elif "open" == message.lower(): #ここから、新システム用コマンド
+				time.sleep(0.2)
+				mouse.right_click()
+				message = ""
+			elif "craft" == message.lower(): 
+				time.sleep(0.2)
+				mouse.right_click()
+				message = ""
+			elif "m" in message.lower():
+				l = message.lower().split(',')
+				print('------------------')
+				print(x,y,width,height) 
+				print('------------------')
+				mouse.move(x+(height-x)*float(l[1]),y+(width-y)*float(l[2]),absolute=True, duration=0.0)
+				print(x+(height-x)*float(l[1]),y+(width-y)*float(l[2]),x,y) 
+				time.sleep(0.3)
+				mouse.right_click()
+				message = ""
+			elif "w" in message.lower():
+				l = message.lower().split(',')
+				keypresser.key_down('w')
+				time.sleep(int(l[1]))
+				keypresser.key_up('w')
+				message = ""
+			elif "a" in message.lower():
+				l = message.lower().split(',')
+				keypresser.key_down('a')
+				time.sleep(int(l[1]))
+				keypresser.key_up('a')
+				message = ""
+			elif "s" in message.lower():
+				l = message.lower().split(',')
+				keypresser.key_down('s')
+				time.sleep(int(l[1]))
+				keypresser.key_up('s')
+				message = ""
+			elif "d" in message.lower():
+				l = message.lower().split(',')
+				keypresser.key_down('d')
+				time.sleep(int(l[1]))
+				keypresser.key_up('d')
+				message = ""
+			elif "vw" in message.lower():
+				l = message.lower().split(',')
+				mouse.move(0, -3*int(l[1]), absolute=False, duration=0.5)
+				message = ""
+			elif "vs" in message.lower():
+				l = message.lower().split(',')
+				mouse.move(0, 3*int(l[1]), absolute=False, duration=0.5)
+				message = ""
+			elif "va" in message.lower():
+				l = message.lower().split(',')
+				mouse.move(-3*int(l[1]), 0, absolute=False, duration=0.5)
+				message = ""
+			elif "vd" in message.lower():
+				l = message.lower().split(',')
+				mouse.move(3*int(l[1]), 0, absolute=False, duration=0.5)
+				message = ""
+			'''
+			elif "cr" == message.lower():
+				time.sleep(0.2)
+				mouse.right_click()
+				message = ""
+			'''
 
 def twitch(): #まるごとコピーして使ったらいい。いつもコメントを取得している
 
